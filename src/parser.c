b642bb536335eaa73916d702d80ba91659410c90
Marko Viitanen <fador@iki.fi>
2026-02-08 22:43:06 +0200

    Fix crashing and add support for structs

--- src/parser.c
+++ src/parser.c
@@ -23,6 +23,8 @@ static void parser_expect(Parser *parser, TokenType type) {
 }
 
 static ASTNode *parse_expression(Parser *parser);
+static ASTNode *parse_struct_body(Parser *parser, char *name);
+static ASTNode *parse_struct_def(Parser *parser);
 
 static Type *parse_type(Parser *parser) {
     Type *type = NULL;
@@ -225,12 +227,66 @@ static ASTNode *parse_statement(Parser *parser) {
         parser_expect(parser, TOKEN_SEMICOLON);
         return node;
     } else if (parser->current_token.type == TOKEN_KEYWORD_INT || parser->current_token.type == TOKEN_KEYWORD_STRUCT) {
-        // Variable or struct declaration
+        // Variable or struct declaration/definition
         if (parser->current_token.type == TOKEN_KEYWORD_STRUCT) {
             Token next = lexer_peek_token(parser->lexer);
-            if (next.type == TOKEN_IDENTIFIER) {
-                Token next_next = lexer_peek_token(parser->lexer); // Wait, I don't have peek_peek
-                // If I advanced, I can't easily peek further.
+            
+            if (next.type == TOKEN_LBRACE) {
+                // Anonymous struct definition: struct { ... }
+                parser_advance(parser); // Consume struct
+                return parse_struct_body(parser, NULL);
+            } else if (next.type == TOKEN_IDENTIFIER) {
+                // Check if it's 'struct Name {'
+                // We advance to identifier
+                 parser_advance(parser); // struct
+                 // Now at Name
+                 char *name = malloc(parser->current_token.length + 1);
+                 strncpy(name, parser->current_token.start, parser->current_token.length);
+                 name[parser->current_token.length] = '\0';
+                 
+                 Token check = lexer_peek_token(parser->lexer);
+                 
+                 if (check.type == TOKEN_LBRACE) {
+                     parser_advance(parser); // Consume Name
+                     return parse_struct_body(parser, name);
+                 }
+                 
+                 // Variable declaration: struct Name ...
+                 // We already consumed 'struct'. Current is 'Name'.
+                 // Create the type manually.
+                 Type *type = type_struct(name);
+                 free(name);
+                 
+                 parser_advance(parser); // Consume Name
+                 
+                 // Now parsing possible pointers *
+                 while (parser->current_token.type == TOKEN_STAR) {
+                    type = type_ptr(type);
+                    parser_advance(parser);
+                 }
+                 
+                 // Now create the VAR_DECL node
+                 ASTNode *node = ast_create_node(AST_VAR_DECL);
+                 node->resolved_type = type;
+                 
+                 if (parser->current_token.type == TOKEN_IDENTIFIER) {
+                    node->data.var_decl.name = malloc(parser->current_token.length + 1);
+                    strncpy(node->data.var_decl.name, parser->current_token.start, parser->current_token.length);
+                    node->data.var_decl.name[parser->current_token.length] = '\0';
+                    parser_advance(parser);
+
+                    if (parser->current_token.type == TOKEN_EQUAL) {
+                        parser_advance(parser);
+                        node->data.var_decl.initializer = parse_expression(parser);
+                    } else {
+                        node->data.var_decl.initializer = NULL;
+                    }
+                    parser_expect(parser, TOKEN_SEMICOLON);
+                    return node;
+                 } else {
+                     printf("Syntax Error: Expected variable name after struct type at line %d\n", parser->current_token.line);
+                     exit(1);
+                 }
             }
         }
 
@@ -243,18 +299,26 @@ static ASTNode *parse_statement(Parser *parser) {
         node->resolved_type = type;
         if (parser->current_token.type == TOKEN_IDENTIFIER) {
             node->data.var_decl.name = malloc(parser->current_token.length + 1);
-            strncpy(node->data.var_decl.name, parser->current_token.start, parser->current_token.length);
+            memcpy(node->data.var_decl.name, parser->current_token.start, parser->current_token.length);
             node->data.var_decl.name[parser->current_token.length] = '\0';
             parser_advance(parser);
-        }
-        if (parser->current_token.type == TOKEN_EQUAL) {
-            parser_advance(parser);
-            node->data.var_decl.initializer = parse_expression(parser);
+
+            if (parser->current_token.type == TOKEN_EQUAL) {
+                parser_advance(parser);
+                node->data.var_decl.initializer = parse_expression(parser);
+            } else {
+                node->data.var_decl.initializer = NULL;
+            }
+            parser_expect(parser, TOKEN_SEMICOLON);
+            return node;
         } else {
-            node->data.var_decl.initializer = NULL;
+            // Not a variable declaration, maybe a stray struct definition or error?
+            // If it was 'struct Point {', parse_type consumed 'struct Point'.
+            // Current token is '{'.
+            // We should ideally error out or backtrack, but for now let's just error
+            printf("Syntax Error: Expected variable name after type at line %d. (Struct definitions inside functions not fully supported yet)\n", parser->current_token.line);
+            exit(1); 
         }
-        parser_expect(parser, TOKEN_SEMICOLON);
-        return node;
     } else if (parser->current_token.type == TOKEN_KEYWORD_WHILE) {
         parser_advance(parser);
         ASTNode *node = ast_create_node(AST_WHILE);
@@ -324,6 +388,42 @@ static ASTNode *parse_function(Parser *parser) {
     return node;
 }
 
+// Helper to parse struct body: { int x; int y; } ;
+static ASTNode *parse_struct_body(Parser *parser, char *name) {
+    Type *struct_type = name ? type_struct(name) : NULL;
+    
+    parser_expect(parser, TOKEN_LBRACE);
+    ASTNode *node = ast_create_node(AST_STRUCT_DEF);
+    node->data.struct_def.name = name;
+    
+    int current_offset = 0;
+    while (parser->current_token.type != TOKEN_RBRACE && parser->current_token.type != TOKEN_EOF) {
+        Type *member_type = parse_type(parser);
+        if (parser->current_token.type == TOKEN_IDENTIFIER) {
+            ASTNode *member = ast_create_node(AST_VAR_DECL);
+            member->data.var_decl.name = malloc(parser->current_token.length + 1);
+            strncpy(member->data.var_decl.name, parser->current_token.start, parser->current_token.length);
+            member->data.var_decl.name[parser->current_token.length] = '\0';
+            parser_advance(parser);
+            ast_add_child(node, member);
+            
+            if (struct_type) {
+                Member *m = &struct_type->data.struct_data.members[struct_type->data.struct_data.members_count++];
+                m->name = strdup(member->data.var_decl.name);
+                m->type = member_type;
+                m->offset = current_offset;
+                current_offset += member_type->size;
+                struct_type->size = current_offset;
+            }
+            
+            parser_expect(parser, TOKEN_SEMICOLON);
+        }
+    }
+    parser_expect(parser, TOKEN_RBRACE);
+    parser_expect(parser, TOKEN_SEMICOLON);
+    return node;
+}
+
 static ASTNode *parse_struct_def(Parser *parser) {
     parser_expect(parser, TOKEN_KEYWORD_STRUCT);
     char *name = NULL;
@@ -334,38 +434,8 @@ static ASTNode *parse_struct_def(Parser *parser) {
         parser_advance(parser);
     }
     
-    Type *struct_type = name ? type_struct(name) : NULL;
-    
     if (parser->current_token.type == TOKEN_LBRACE) {
-        parser_advance(parser);
-        ASTNode *node = ast_create_node(AST_STRUCT_DEF);
-        node->data.struct_def.name = name;
-        int current_offset = 0;
-        while (parser->current_token.type != TOKEN_RBRACE && parser->current_token.type != TOKEN_EOF) {
-            Type *member_type = parse_type(parser);
-            if (parser->current_token.type == TOKEN_IDENTIFIER) {
-                ASTNode *member = ast_create_node(AST_VAR_DECL);
-                member->data.var_decl.name = malloc(parser->current_token.length + 1);
-                strncpy(member->data.var_decl.name, parser->current_token.start, parser->current_token.length);
-                member->data.var_decl.name[parser->current_token.length] = '\0';
-                parser_advance(parser);
-                ast_add_child(node, member);
-                
-                if (struct_type) {
-                    Member *m = &struct_type->data.struct_data.members[struct_type->data.struct_data.members_count++];
-                    m->name = strdup(member->data.var_decl.name);
-                    m->type = member_type;
-                    m->offset = current_offset;
-                    current_offset += member_type->size;
-                    struct_type->size = current_offset;
-                }
-                
-                parser_expect(parser, TOKEN_SEMICOLON);
-            }
-        }
-        parser_expect(parser, TOKEN_RBRACE);
-        parser_expect(parser, TOKEN_SEMICOLON);
-        return node;
+        return parse_struct_body(parser, name);
     }
     return NULL;
 }
